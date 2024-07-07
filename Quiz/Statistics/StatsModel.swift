6d646b433b531fd298445a7d68ae30618874de3e
SandroBoka <sandro.boka1@gmail.com>
2024-07-07 21:30:28 +0200

    StatsView and StatsModel added

---
+++ Quiz/Statistics/StatsModel.swift
@@ -0,0 +1,16 @@
+//
+//  StatsModel.swift
+//  Quiz
+//
+//  Created by Sandro Boka on 07.07.2024..
+//
+
+import Foundation
+
+struct StatsModel {
+    let numAnswered: Int
+    let numCorrect: Int
+    let bestScore: Int
+    let dashNum: Int
+    let normalNum: Int
+}
