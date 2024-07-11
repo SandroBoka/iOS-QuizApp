a439d677abf12dfa26e668910c5dd0402bd45272
SandroBoka <sandro.boka1@gmail.com>
2024-07-11 18:29:00 +0200

    New colors added

--- Quiz/Extensions/Extensions.swift
+++ Quiz/Extensions/Extensions.swift
@@ -13,6 +13,12 @@ extension Color {
     static let backgroundColor = Color(red:255/255, green: 255/255, blue: 221/255)
     static let buttonsColor = Color(red:21/255, green: 205/255, blue: 150/255)
     
+//      static let headerColor = Color(red: 64/255, green: 165/255, blue: 193/255)
+//      static let headerColor = Color(red: 77/255, green: 193/255, blue: 220/255)
+//      static let headerColor = Color(red: 48/255, green: 187/255, blue: 220/255)
+//      static let backgroundColor = Color(red:255/255, green: 255/255, blue: 221/255)
+//      static let buttonsColor = Color(red:21/255, green: 205/255, blue: 150/255)
+    
 //    static let headerColor = Color(red:0/255, green: 128/255, blue: 129/255)//tirkizna tamnija
 //    static let backgroundColor = Color(red:255/255, green: 255/255, blue: 221/255)// bijelo zuta
 //    static let buttonsColor = Color(red:21/255, green: 205/255, blue: 150/255)//svjetlo zelena
